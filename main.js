fa87b4796b4f71ea19cbb13d17be73d8f0c7eb25
Brandon Mitchell <brandonmitchell217@gmail.com>
2021-10-24 01:22:17 -0400

    First commit.

---
+++ main.js
@@ -0,0 +1,76 @@
+const amberBtn = document.querySelector('.amberBtn')
+const amberSlide = document.querySelector('.amberSlide')
+const brownBtn = document.querySelector('.brownBtn')
+const brownSlide = document.querySelector('.brownSlide')
+const hoppyBtn = document.querySelector('.hoppyBtn')
+const hoppySlide = document.querySelector('.hoppySlide')
+
+// amberBtn.addEventListener('click', () => {
+//   amberSlide.style.transform = 'translate(0, -50%)'
+// })
+// brownBtn.addEventListener('click', () => {
+//   brownSlide.style.transform = 'translate(0, -50%)'
+// })
+// hoppyBtn.addEventListener('click', () => {
+//   hoppySlide.style.transform = 'translate(0, -50%)'
+// })
+
+const selectionBtns = document.querySelectorAll('.selectionBtn')
+const btnArray = Array.from(selectionBtns)
+const slides = document.querySelectorAll('.slide')
+
+window.addEventListener('DOMContentLoaded', () => {
+  selectionBtns[0].classList.add('active')
+})
+
+const tl = gsap.timeline({ delay: 1.5 })
+// tl.from('.slides', { duration: 1.25, x: '100%' })
+// tl.from('.title', { duration: 1.5, opacity: 0 })
+// tl.from('.content', { duration: 1.5, opacity: 0 }, '-=1.5')
+// tl.from('.abv', { duration: 1.5, opacity: 0 }, '-=1.5')
+// tl.from('.redRec', { duration: 1, x: '100%' }, '-=1')
+// tl.from('.beer', { duration: 1.5, x: '100%', opacity: 0 }, '-=0.5')
+
+for (let i = 0; i < selectionBtns.length && i < slides.length; i++) {
+  slides[i] === selectionBtns[i]
+  selectionBtns[i].addEventListener('click', () => {
+    selectionBtns.forEach((btn) => {
+      btn.classList.remove('active')
+    })
+    if (selectionBtns[i].classList.contains('active')) {
+      selectionBtns[i].classList.remove('active')
+    } else {
+      selectionBtns[i].classList.add('active')
+    }
+    slides.forEach((slide) => {
+      slide.style.transform = 'translate(160%, -50%)'
+    })
+    slides[i].style.transform = 'translate(0, -50%)'
+    tl.from('.title', { duration: 1.5, opacity: 0 })
+    tl.from('.content', { duration: 1.5, opacity: 0 }, '-=1.5')
+    tl.from('.abv', { duration: 1.5, opacity: 0 }, '-=1.5')
+    tl.from('.redRec', { duration: 1, x: '100%' }, '-=1')
+    tl.from('.beer', { duration: 1.5, x: '100%', opacity: 0 }, '-=0.5')
+  })
+}
+// for (let i = 0; i < selectionBtns.length; i++) {
+//   selectionBtns[i].addEventListener('click', () => {
+//     selectionBtns.forEach((btn) => {
+//       btn.classList.remove('active')
+//     })
+//     if (selectionBtns[i].classList.contains('active')) {
+//       selectionBtns[i].classList.remove('active')
+//     } else {
+//       selectionBtns[i].classList.add('active')
+//     }
+//   })
+//   selectionBtns[i].addEventListener('click', (e) => {
+//     console.log(e.target)
+//   })
+// }
+
+// function slideSelect(e) {
+//    for (let i = 0; i < slides.length; i++) {
+
+//    }
+// }
