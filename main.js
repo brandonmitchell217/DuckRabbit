cf12d1f28f60d5eeed8ca2dda5c2a21b3a4dd5cc
Brandon Mitchell <brandonmitchell217@gmail.com>
2021-10-25 08:41:48 -0400

    Added base level GSAP

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+gsap.registerPlugin(ScrollTrigger)
 const amberBtn = document.querySelector('.amberBtn')
 const amberSlide = document.querySelector('.amberSlide')
 const brownBtn = document.querySelector('.brownBtn')
@@ -23,13 +24,22 @@ window.addEventListener('DOMContentLoaded', () => {
   selectionBtns[0].classList.add('active')
 })
 
-const tl = gsap.timeline({ delay: 1.5 })
-// tl.from('.slides', { duration: 1.25, x: '100%' })
-// tl.from('.title', { duration: 1.5, opacity: 0 })
-// tl.from('.content', { duration: 1.5, opacity: 0 }, '-=1.5')
-// tl.from('.abv', { duration: 1.5, opacity: 0 }, '-=1.5')
-// tl.from('.redRec', { duration: 1, x: '100%' }, '-=1')
-// tl.from('.beer', { duration: 1.5, x: '100%', opacity: 0 }, '-=0.5')
+gsap.from('.stoutSlide', {
+  scrollTrigger: '#beers',
+  start: 'top top',
+  end: 'bottom bottom',
+  opacity: 0,
+  x: 500,
+  anticipatePin: 1,
+})
+gsap.from('.selections button', {
+  scrollTrigger: '#beers',
+  start: 'bottom bottom',
+  end: 'bottom bottom',
+  x: -500,
+  stagger: 0.5,
+  anticipatePin: 1,
+})
 
 for (let i = 0; i < selectionBtns.length && i < slides.length; i++) {
   slides[i] === selectionBtns[i]
@@ -46,11 +56,12 @@ for (let i = 0; i < selectionBtns.length && i < slides.length; i++) {
       slide.style.transform = 'translate(160%, -50%)'
     })
     slides[i].style.transform = 'translate(0, -50%)'
-    tl.from('.title', { duration: 1.5, opacity: 0 })
-    tl.from('.content', { duration: 1.5, opacity: 0 }, '-=1.5')
-    tl.from('.abv', { duration: 1.5, opacity: 0 }, '-=1.5')
+    const tl = gsap.timeline({ delay: 0.25 })
+    tl.from('.title', { duration: 1.5, opacity: 0, x: 300 })
     tl.from('.redRec', { duration: 1, x: '100%' }, '-=1')
-    tl.from('.beer', { duration: 1.5, x: '100%', opacity: 0 }, '-=0.5')
+    tl.from('.content', { duration: 1.5, opacity: 0, x: 300 }, '-=1.5')
+    tl.from('.beer', { duration: 1.5, x: '100%', opacity: 0 }, '-=1.2')
+    tl.from('.abv', { duration: 1.5, opacity: 0 }, '-=1')
   })
 }
 // for (let i = 0; i < selectionBtns.length; i++) {
